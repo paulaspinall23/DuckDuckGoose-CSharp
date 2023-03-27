63446948f1a39607dae0d240562c8d6dfc454f0d
Tim Leach <timleach635@gmail.com>
2023-03-27 10:47:09 +0100

    Add "honk" page

--- DuckDuckGoose/Controllers/HonkController.cs
+++ DuckDuckGoose/Controllers/HonkController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DuckDuckGoose.Areas.Identity.Data;
+using DuckDuckGoose.Models.Requests;
+using Microsoft.AspNetCore.Mvc;
 using DuckDuckGoose.Repositories;
+using Microsoft.AspNetCore.Identity;
 
 namespace DuckDuckGoose.Controllers;
 
@@ -8,14 +11,37 @@ public class HonkController : Controller
 {
     private readonly ILogger<HonkController> _logger;
     private readonly IHonkRepo _honks;
+    private readonly UserManager<DuckDuckGooseUser> _userManager;
+    private readonly IHttpContextAccessor _httpContext;
 
     public HonkController
     (
         ILogger<HonkController> logger,
-        IHonkRepo honks
+        IHonkRepo honks,
+        UserManager<DuckDuckGooseUser> userManager,
+        IHttpContextAccessor httpContext
     )
     {
         _logger = logger;
         _honks = honks;
+        _userManager = userManager;
+        _httpContext = httpContext;
+    }
+
+    [HttpGet("new")]
+    public IActionResult CreateHonkForm()
+    {
+        return View();
+    }
+
+    [HttpPost("")]
+    public async Task<IActionResult> CreateHonk([FromForm] CreateHonkRequest newHonkRequest)
+    {
+        var user = await _userManager.GetUserAsync(_httpContext.HttpContext?.User);
+        string userId = user.Id;
+
+        _honks.CreateHonk(newHonkRequest.Content, userId);
+
+        return RedirectToAction("Index");
     }
 }
--- DuckDuckGoose/Repositories/HonkRepo.cs
+++ DuckDuckGoose/Repositories/HonkRepo.cs
@@ -1,13 +1,11 @@
 using DuckDuckGoose.Areas.Identity.Data;
-using DuckDuckGoose.Models;
 using DuckDuckGoose.Models.Database;
-using DuckDuckGoose.Models.Requests;
-using Microsoft.EntityFrameworkCore;
 
 namespace DuckDuckGoose.Repositories;
 
 public interface IHonkRepo
 {
+    public Honk CreateHonk(string content, string userId);
 }
 
 public class HonkRepo : IHonkRepo
@@ -17,5 +15,31 @@ public class HonkRepo : IHonkRepo
     public HonkRepo(DuckDuckGooseIdentityDbContext context)
     {
         _context = context;
+    }
+
+    public Honk CreateHonk(string content, string userId)
+    {
+        DuckDuckGooseUser user;
+
+        try
+        {
+            user = _context.Users
+                .Single(u => u.Id == userId);
+        }
+        catch (Exception e)
+        {
+            throw new ArgumentException($"Cannot find user with id {userId}", e);
+        }
+
+        var newHonk = new Honk
+        {
+            Content = content,
+            Timestamp = DateTime.Now.ToUniversalTime(),
+            User = user,
+        };
+
+        var addedEntity = _context.Honks.Add(newHonk);
+
+        return addedEntity.Entity;
     }
 }
